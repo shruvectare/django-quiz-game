6f24a8f0abbf404ee10ba2ae29460aa753d35257
shruvectare <shrutika.pedamkar@vectare.co.uk>
2023-10-12 16:04:20 +0100

    Adding files

--- quizApp/tests.py
+++ quizApp/tests.py
@@ -46,40 +46,27 @@ class test_Task (unittest.TestCase):
         s = response.context['score']
         self.assertEqual(s, 50)
     #
-    # def test_submit_some_correct_answers(self):
-    #     # Simulate a POST request with correct answers
-    #     questions = quizQuestions.objects.all()
-    #     self.client = Client()
-    #     response = self.client.post(reverse('home'), {
-    #         "Which one is not a Hogwarts house?": "Ravenclaw",
-    #         "What spell did Harry use to kill Lord Voldemort?": "Expelliarmus",
-    #         "Where does Hermione brew her first batch of Polyjuice Potion?": "Moaning Myrtle’s Bathroom",
-    #         "From what King’s Cross platform does the Hogwarts Express leave?": "Nine and Three-quarters",
-    #         "What position does Harry play on the Gryffindor Quidditch team?": "Keeper"
-    #     })
-    #
-    #     assert response.status_code == 200  # Check the page redirects you (HTTP 302
-    #     assert "score" in response.context
-    #     s = response.context['score']
-    #     self.assertEqual(s, 30)
-
-
-
-
-
-            assert quiz.question in str(response.content.decode("utf-8"))
-
-    def test_submit_all_correct_answers(self):
+    def test_submit_some_correct_answers(self):
         # Simulate a POST request with correct answers
         questions = quizQuestions.objects.all()
         self.client = Client()
         response = self.client.post(reverse('home'), {
-            "Which one is not a Hogwarts house?" : "Dragonheart",
-            "What spell did Harry use to kill Lord Voldemort?" : "Expelliarmus",
-            "Where does Hermione brew her first batch of Polyjuice Potion?" : "Moaning Myrtle’s Bathroom",
-            "From what King’s Cross platform does the Hogwarts Express leave?" : "Nine and Three-quarters",
-            "What position does Harry play on the Gryffindor Quidditch team?" : "Seeker"
+            "Which one is not a Hogwarts house?": "Ravenclaw",
+            "What spell did Harry use to kill Lord Voldemort?": "Expelliarmus",
+            "Where does Hermione brew her first batch of Polyjuice Potion?": "Moaning Myrtle’s Bathroom",
+            "From what King’s Cross platform does the Hogwarts Express leave?": "Nine and Three-quarters",
+            "What position does Harry play on the Gryffindor Quidditch team?": "Keeper"
         })
+
+        assert response.status_code == 200  # Check the page redirects you (HTTP 302
+        assert "score" in response.context
+        s = response.context['score']
+        self.assertEqual(s, 30)
+
+
+
+
+
 
 
 
